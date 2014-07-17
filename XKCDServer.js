b0a5e3f85b073f7af3dadd3baa554dafd815296e
fahmidur <s.f.reza@gmail.com>
2014-07-17 15:56:41 -0400

    improved node server

--- XKCDServer.js
+++ XKCDServer.js
@@ -10,30 +10,78 @@ var xkcd = (function() {
 	var infoPart = '/info.0.json';
 
 	var latestURL = base + infoPart;
+	var errorJSON = JSON.stringify({error: true});
 
-	function getURL(url, res, id) {
-		if(id && cache[id]) { return res.end(cache[id]); }
-		console.log('NEW REQUEST');
-		request({url: url, json: true}, function(error, response, body) {
+	function getURL(opt) {
+		if(opt.id && cache[opt.id]) { return opt.res.end(cache[opt.id]); }
+
+		console.log('NEW REQUEST', new Date());
+		request({url: opt.url, json: true}, function(error, response, body) {
 			if(!error && response.statusCode === 200) {
 				cache[body.num] = JSON.stringify(body);
-				res.end(cache[body.num]);
+				// res.end(cache[body.num]);
+				opt.successCallback(opt.res, body);
 			} else {
-				res.end(JSON.stringify({error: true}));
+				opt.failureCallback(opt.res);
+				// res.end(JSON.stringify({error: true}));
 			}
 		});
 	};
-	function getID(req, res) {
-		getURL(base + '/' + req.params.id + infoPart, res, req.params.id);
 
+	function bodyWriter(res, body) {
+		res.end(JSON.stringify(body));
 	};
+
+	function failWriter(res) {
+		res.end(errorJSON);
+	};
+
+	//--------------------------------
+	// PUBLIC
+	//--------------------------------
+	function getID(req, res) {
+		getURL({
+			id: req.params.id, // cache
+			res: res,
+			url: base + '/' + req.params.id + infoPart,
+			successCallback: bodyWriter,
+			failureCallback: failWriter
+		});
+	};
+
 	function getLatest(req, res) {
-		getURL(latestURL, res);
+		getURL({
+			res: res,
+			url: latestURL,
+			successCallback: bodyWriter,
+			failureCallback: failWriter
+		});
 	};
+
+	function getRandom(req, res) {
+		getURL({
+			res: res,
+			url: latestURL,
+			successCallback: function(res2, body) {
+				var latestNum = body.num;
+				var randomID = ~~(Math.random() * latestNum)+1;
+				// res.end(JSON.stringify({latestNum: latestNum, randomID: randomID}));
+				getURL({
+					id: randomID, //cache
+					res: res,
+					url: base + '/' + randomID + infoPart,
+					successCallback: bodyWriter,
+					failureCallback: failWriter
+				});
+			},
+			failureCallback: failWriter
+		});	
+	}
 
 	return {
 		getLatest: getLatest,
-		getID: getID
+		getID: getID,
+		getRandom: getRandom,
 	};
 })();
 
@@ -48,6 +96,7 @@ function allowAccess(req, res, next) {
 };
 
 app.get('/latest', allowAccess, xkcd.getLatest);
+app.get('/random', allowAccess, xkcd.getRandom);
 app.get('/:id(\\d+)', allowAccess, xkcd.getID);
 
 app.listen(app.get('port'), function() {
--- js/XKCD_Embedder.js
+++ js/XKCD_Embedder.js
@@ -1,3 +1,11 @@
-var XKCD_Embedder = function(server_url) {
-		
-};
+var XKCD_Embedder = (function(serverURL) {
+	var ar = document.querySelectorAll('.xkcd-embed');
+	for(var i in ar) { var v = ar[i];
+		var id = v.getAttribute('data-id');
+		// console.log(ar[i], id);
+		$.getJSON(serverURL + '/' + id, function(data) {
+			console.log(data);
+		});
+		// v.innerHTML = "<img src=''></img";
+	}
+})('http://xkcd-embedder.fahmidur.us');
