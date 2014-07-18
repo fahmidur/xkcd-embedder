d75717f8a3909949b32d23e2202eda9588883df5
fahmidur <s.f.reza@gmail.com>
2014-07-18 02:47:16 -0400

    progress

--- js/XKCD_Embedder.js
+++ js/XKCD_Embedder.js
@@ -1,4 +1,5 @@
 var XKCD = function(element, serverURL) {
+	this.countID = ++XKCD.count;
 	this.element = element;
 	this.data = element.dataset;
 	this.url = serverURL + '/' + this.data.id;
@@ -7,20 +8,63 @@ var XKCD = function(element, serverURL) {
 
 	//LOAD the XKCD
 	XKCD_Embedder.getJSON(this.url, function(data) {
-		console.log(data, data.img);
-		self.element.innerHTML += "<div class='xkcd-embed-title'>"+data.title+"</div>";
-		self.element.innerHTML += "<div class='xkcd-embed-comic'><img src='"+data.img+"' title=\""+data.alt+"\"></img></div>";
-		// self.element.innerHTML += "<div class='xkcd-embed-alt'>"+data.alt+"</div>";
+		console.log(data, data.img, data.num);
+		var top = document.createElement('div');
+		top.className = 'xkcd-embed-top';
+		element.appendChild(top);
+
+		var title = document.createElement("a");
+		title.className = 'xkcd-embed-title';
+		title.href = "http://xkcd.com/"+data.num;
+		title.target = '_blank';
+		title.textContent = data.title;
+		top.appendChild(title);
+
+		var number = document.createElement('span');
+		number.className = 'xkcd-embed-number';
+		number.textContent = data.num;
+		top.appendChild(number);
+
+		var imgWrapper = document.createElement('div');
+		imgWrapper.className = 'xkcd-embed-comic';
+		element.appendChild(imgWrapper);
+
+		var img = document.createElement('img');
+		img.src = data.img;
+		img.id = 'xkcd-embed-img-'+self.countID;
+		imgWrapper.appendChild(img);
+
+		window.setTimeout(function() {
+				var imgWidth = img.clientWidth;
+
+				var alt = document.createElement("div");
+				alt.className = 'xkcd-embed-alt';
+				alt.textContent = data.alt;
+				alt.style.width = imgWidth - 10;
+				alt.style.display = "none";
+
+				element.appendChild(alt);
+
+				imgWrapper.addEventListener("click", function() {
+					if(alt.style.display === "none") {
+						alt.style.display = '';
+					} else {
+						alt.style.display = "none";
+					}
+				});
+		}, 5000);
+
+		
 	}, function() {
 		console.log('Failed to load resource: ', this.url);
 	});
 };
+XKCD.count = 0;
 
 var XKCD_Embedder = function(serverURL) {
 	var ar = document.querySelectorAll('.xkcd-embed');
 	var xkcds = new Array();
-
-	for(var i = 0; i < ar. length; ++i) { 
+	for(var i = 0; i < ar. length; ++i) {
 		xkcds.push(new XKCD(ar[i], serverURL));
 	}
 };
@@ -37,7 +81,7 @@ XKCD_Embedder.getJSON = function(url, successCallback, failureCallback) {
 			}
 		}
 	};
-	xmlhttp.open("GET", url, true);
+	xmlhttp.open("GET", url, false);
 	xmlhttp.send();
 };
 
