937f085996788d6d65d2c0f098012248b07f2e37
Syed Reza <s.f.reza@gmail.com>
2014-07-29 18:08:14 +0000

    user model and etc

--- xkcd-server.js
+++ xkcd-server.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var request = require('request');
 var prettyjson = require('prettyjson');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var models = require('./models');
+
 
 var app = express();
+
+var mongodbAddress = 'localhost';
+var mongodbName = 'xkcd-embedder';
+
+mongoose.connect('mongodb://' + mongodbAddress + '/' + mongodbName);
+
+
+app.use(session({
+	secret: 'xkcd-is-awseome',
+	store: new MongoStore({
+		url: 'mongodb://' + mongodbAddress,
+		db: mongodbName
+	})
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 var xkcd = (function() {
 	var cache = {};
@@ -75,10 +97,28 @@ var xkcd = (function() {
 		});	
 	}
 
+	function register(req, res) {
+		var email = req.body.email;
+		var password = req.body.password;
+
+		var user = new models.User({
+			email: email,
+		});
+		user.password = password;
+		user.save(function(err) {
+			if(err) { 
+				res.end('Error saving user: ' + err);
+			} else {
+				res.end('User saved: ' + user);
+			}
+		});
+	}
+
 	return {
 		getLatest: getLatest,
 		getID: getID,
 		getRandom: getRandom,
+		register: register
 	};
 })();
 
@@ -99,6 +139,8 @@ app.use('/css', allowAccess, express.static(__dirname + '/public/css'));
 app.get('/latest', allowAccess, xkcd.getLatest);
 app.get('/random', allowAccess, xkcd.getRandom);
 app.get('/:id(\\d+)', allowAccess, xkcd.getID);
+app.post('/register', allowAccess, xkcd.register);
+
 
 app.listen(app.get('port'), function() {
 	console.log('Listening on http://localhost:' + app.get('port'));
