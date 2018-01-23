0b57eb6ade88a1073000681f5c84370cc4d8cec1
Syed Reza <s.f.reza@gmail.com>
2018-01-23 12:40:24 -0800

    post-refactor cleanup

--- models/index.js
+++ models/index.js
@@ -1,14 +1,3 @@
-/*
-module.exports = function(sharedLibs) {
-  var exp = {};
-
-  exp.User = require('./user.js')(sharedLibs, exp);
-  exp.Comic = require('./comic.js')(sharedLibs, exp);
-  exp.Favorite = require('./favorite.js')(sharedLibs, exp);
-  return exp;
-};
-*/
-
 var exp = {};
 exp.User = require('./user');
 exp.Comic = require('./comic');
