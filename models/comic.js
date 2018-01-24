88f2603da46a08852f1c6474148a137b7f83ff63
Syed Reza <s.f.reza@gmail.com>
2018-01-24 11:13:42 -0800

    models.Comic refactor

--- models/comic.js
+++ models/comic.js
@@ -1,19 +1,41 @@
 var request = require('request');
 var bookshelfer = require('../bookshelfer');
 
+
+//---
+
+var classProps = {};
+classProps.fetchers = {};
+classProps.fetchers.xkcd = function fetch_xkcd(ident, cbSuccess, cbFailure) {
+  var url = 'http://xkcd.com/' + (ident == 'latest' ? '' : (ident+'/')) + 'info.0.json';
+  request({url: url, json: true}, function(err, resp, body) {
+    if(err || resp.statusCode !== 200) {
+      console.error('fetchers.xkcd. failure. err = ', errr, "\n---");
+      return cbFailure && cbFailure(err, resp);
+    }
+    console.log('fetchers.xkcd. success. body = ', body, "\n---");
+    return cbSuccess && cbSuccess(body);
+  });
+};
+classProps.fetch = function fetch(name, ident, cbSuccess, cbFailure) {
+  var fetcher = classProps.fetchers[name];
+  if(!fetcher) {
+    throw "No such fetcher";
+  }
+  return fetcher(ident, cbSuccess, cbFailure);
+};
+
+//---
+
 var protoProps = {};
 protoProps.tableName = 'comics';
 protoProps.hasTimestamps = true;
-
-var classProps = {};
-classProps.fetchRemote = function(ident, cbSuccess, cbFailure) {
-  var url = 'http://xkcd.com/' + (ident == 'latest' ? '' : (ident+'/')) + 'info.0.json';
-  request({url: url, json: true}, function(err, resp, body) {
-    if(err || response.statusCode !== 200) {
-      return cbFailure && cbFailure(err, resp);
-    }
-    return cbSucess && cbSuccess(body);
-  });
+protoProps.getErrors = function() {
+  var self = this;
+  if(!self.get('source')) {
+  }
 };
+
+//---
 
 module.exports = bookshelfer(protoProps, classProps);
