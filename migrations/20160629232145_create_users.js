df11899bc7f7596be0bedbdda025132207e1f998
Syed Reza <s.f.reza@gmail.com>
2021-03-21 11:28:49 -0700

    wip

--- migrations/20160629232145_create_users.js
+++ migrations/20160629232145_create_users.js
@@ -1,23 +1,19 @@
 exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('users', function(table) {
-      table.increments();
-      table.string('email').unique();
-      table.string('password_bcrypt');
-      table.string('password_reset_token').unique();
-      table.boolean('email_verified');
-      table.string('email_verification_token').unique();
-      table.string("login_ip_current");
-      table.string("login_ip_last");
-      table.dateTime("login_time_current");
-      table.dateTime("login_time_last");
-      table.timestamps();
-    })
-  ]);
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.string('email').unique();
+    table.string('password_bcrypt');
+    table.string('password_reset_token').unique();
+    table.boolean('email_verified');
+    table.string('email_verification_token').unique();
+    table.string("login_ip_current");
+    table.string("login_ip_last");
+    table.dateTime("login_time_current");
+    table.dateTime("login_time_last");
+    table.timestamps();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('users')
-  ]);
+  return knex.schema.dropTable('users')
 };
